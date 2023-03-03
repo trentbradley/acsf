5a38bc84a554bc3ec99b6fbc41ece53b1899e9d7
t <t@localhost>
2023-03-03 13:32:57 -0600

    ORDS: Week 1 Challenge completed

--- Ordered Data Structures/Week 1/Challenge/Challenge.cpp
+++ Ordered Data Structures/Week 1/Challenge/Challenge.cpp
@@ -5,17 +5,29 @@
 std::stack<int> reverse_stack(std::stack<int> s) {
     std::stack<int> reversed_s;
 
-    // write code here that returns a stack whose elements are
-    // in reverse order from those in stack s
+    while(!s.empty())
+    {
+        reversed_s.push(s.top());
+        s.pop();
+    }
 
     return reversed_s;
 }
 
 std::queue<int> reverse_queue(std::queue<int> q) {
     std::queue<int> reversed_q;
+    std::stack<int> temp;
+    while(!q.empty())
+    {
+        temp.push(q.front());
+        q.pop();
+    }
 
-    // write code here that returns a queue whose elements are
-    // in reverse order from those in queue q
+    while(!temp.empty())
+    {
+        reversed_q.push(temp.top());
+        temp.pop();
+    }
 
     return reversed_q;
 }
